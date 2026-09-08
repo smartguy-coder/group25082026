156614ff7d2cb1b443f3867bfb88c438c570e232
Vasyl Kartychak <68468261+smartguy-coder@users.noreply.github.com>
2026-09-08 21:14:11 +0300

    testing fixtures (#7)
    * testing fixtures
    
    * testing fixtures
    
    * testing fixtures
    
    * actions tests
    
    * actions tests
    
    * actions tests
    
    * actions tests
    
    ---------
    
    Co-authored-by: Vasyl <Axel@gmail.com>

--- app/project/tests/test_model_bank_account_1.py
+++ app/project/tests/test_model_bank_account_1.py
@@ -18,3 +18,25 @@ class TestBankAccountATMMashine:
         bank_account.deposit_money(100)
         bank_account.withdraw_money(100)
         assert bank_account.money == 0
+
+
+class TestBankAccountATMMashineOptimal:
+    def test_deposit(self, bank_account: BankAccount, deposit_amount_100: int):
+        bank_account.deposit_money(deposit_amount_100)
+        bank_account.deposit_money(200)
+        assert bank_account.money == 300
+
+    def test_withdraw(self, bank_account: BankAccount):
+        bank_account.withdraw_money(100)
+        assert bank_account.money == 200
+
+    def test_withdraw_and_deposit(self, bank_account: BankAccount, deposit_amount_100: int):
+        bank_account.deposit_money(deposit_amount_100)
+        bank_account.withdraw_money(100)
+        assert bank_account.money == 200
+
+
+def test_deposit(bank_account: BankAccount, deposit_amount_100: int):
+    bank_account.deposit_money(deposit_amount_100)
+    bank_account.deposit_money(200)
+    assert bank_account.money == 500
