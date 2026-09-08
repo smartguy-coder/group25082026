ee3b6fd5837eacb79da4d51861d6ccd53d273c2e
Vasyl <Axel@gmail.com>
2026-09-08 21:13:16 +0300

    actions tests

--- app/project/models.py
+++ app/project/models.py
@@ -7,7 +7,6 @@ class BankAccount:
         self.id = uuid.uuid7()  # UUID('01a06d8c-4402-71b2-a90e-5f764e656bdc')
 
     def withdraw_money(self, summa: int) -> None:
-        1/0
         self.money -= summa
         print(f"SMS: {self.id} withdraw_money {summa}. Current balance: {self.money}grn")
 
